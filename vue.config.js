bdebec87fd912f7af9970cae3b07d89c461484f7
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-05-27 19:53:27 +0200

    Add analyser

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new BundleAnalyzerPlugin(),
+    ],
+  },
+};
