a52a72a41d8397b748b8f2c8a8ea4208fd80877a
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-05-28 12:04:55 +0200

    Analyze only in dev

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 // eslint-disable-next-line
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
+const plugins = [];
+
+if (process.env.NODE_ENV !== 'production') {
+  plugins.push(new BundleAnalyzerPlugin());
+}
+
 module.exports = {
   configureWebpack: {
-    plugins: [
-      new BundleAnalyzerPlugin(),
-    ],
+    plugins,
   },
 };
