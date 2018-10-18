dc6f7275a57238b2b965df6000dcd9e4a36a0f29
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-10-18 22:40:58 +0200

    Fix test and coverage

--- src/components/VColorPickerInput.spec.js
+++ src/components/VColorPickerInput.spec.js
@@ -1,5 +1,13 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 import { mount } from '@vue/test-utils';
 import VColorPickerInput from './VColorPickerInput';
+
+// Handle Vuetify and the root element
+Vue.use(Vuetify);
+const rootAppElement = document.createElement('div');
+rootAppElement.setAttribute('data-app', true);
+document.body.appendChild(rootAppElement);
 
 describe('VColorPickerInput', () => {
   it('is a Vue instance', () => {
